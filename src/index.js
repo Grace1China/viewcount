31f82e5b97686e1162bac1c52258ee4522e7a513
graceword <graceword@qq.com>
2023-05-29 20:31:21 +0800

    some comments

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 module.exports = function (router, { services, exceptions }) {
 	const { ItemsService, AuthenticationServicere } = services;
-	// console.log('services', services)
 	const { ServiceUnavailableException } = exceptions;
 	router.get('/:collections/:id', (req, res, next) => {
-		// console.log(req.params.id)
-
 		const collections = req.params['collections'] || '';
 		const id = req.params['id'] || '';
 		const sv = new ItemsService(collections, { schema: req.schema, accountability: req.accountability });
-		// console.log('collections, id,req,resåå', collections, id, req, res)
-
 		sv.readOne(id, {
 			fields: [
 				"*",
@@ -20,7 +15,5 @@ module.exports = function (router, { services, exceptions }) {
 		}).catch((error) => {
 			return next(new ServiceUnavailableException(error.message));
 		});
-
-		// res.send('Hello, World!')
 	});
 };
